e410b97e9ef0029e1e1703cc31df39851d337ddb
AlainMk <alainmukingiro.am@gmail.com>
2019-05-23 22:26:30 +0100

    je viens d'ajouter les différent packages et libreries qu'on pourra utiliser dans le projet

--- app/src/main/java/com/llab/ligablo/controllers/activities/MainActivity.java
+++ app/src/main/java/com/llab/ligablo/controllers/activities/MainActivity.java
@@ -1,7 +1,9 @@
-package com.llab.ligablo;
+package com.llab.ligablo.controllers.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.llab.ligablo.R;
 
 public class MainActivity extends AppCompatActivity {
 

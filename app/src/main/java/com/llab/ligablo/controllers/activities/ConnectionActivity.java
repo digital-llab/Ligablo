d15a54deb85c49d71aefb0057f4a5c480fe87311
AlainMk <alainmukingiro.am@gmail.com>
2019-06-09 20:16:23 +0100

    Aide faite à Jacob pour ButterKnife

--- app/src/main/java/com/llab/ligablo/controllers/activities/ConnectionActivity.java
+++ app/src/main/java/com/llab/ligablo/controllers/activities/ConnectionActivity.java
@@ -3,29 +3,33 @@ package com.llab.ligablo.controllers.activities;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
 import com.llab.ligablo.R;
 
+import butterknife.BindView;
+import butterknife.OnClick;
+
 public class ConnectionActivity extends AppCompatActivity {
+
+    @BindView(R.id.btnMotDePasseOublie) Button btnMotDePasseOublie;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_connection);
-//retrouver le bouton de validation et gerer l'evenement une fois le bouton cliqué
-        Button btnValidationConnection= findViewById(R.id.btnConnectionValider);
-        Button btnMotDePasseOublié= findViewById(R.id.btnMotDePasseOublié);
-        btnValidationConnection.setOnClickListener((View v) -> {
-            Toast.makeText(ConnectionActivity.this, " Button de validation cliqué ", Toast.LENGTH_LONG).show();
-
-btnMotDePasseOublié.setVisibility(Button.VISIBLE);
-            Intent dashBoard = new Intent(this, DashBoard.class);
-            startActivity(dashBoard);
-        });
-
 
     }
+
+
+    @OnClick(R.id.btnConnectionValider)
+    public void onClickConnectionValider() {
+
+        Toast.makeText(ConnectionActivity.this, " Button de validation cliqué ", Toast.LENGTH_LONG).show();
+        btnMotDePasseOublie.setVisibility(Button.VISIBLE);
+        Intent dashBoard = new Intent(this, DashBoard.class);
+        startActivity(dashBoard);
+    }
+
 }
